c0f4f027771609ec02d96cd96dda35595578c2c3
Michal Jenis <michal.jenis@kosik.cz>
2021-05-12 11:17:09 +0200

    Dispatch message via MessageBus

--- project/app/Presenters/HomepagePresenter.php
+++ project/app/Presenters/HomepagePresenter.php
@@ -4,9 +4,24 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
+use App\Messenger\Message\SmsNotification;
+use App\Messenger\MessageBus;
 use Nette;
-
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+
+	private MessageBus $bus;
+
+	public function __construct(MessageBus $bus)
+	{
+		parent::__construct();
+		$this->bus = $bus;
+	}
+
+	public function actionDefault(): void
+	{
+		$this->bus->dispatch(new SmsNotification('Look! I created a message!'));
+	}
+
 }
